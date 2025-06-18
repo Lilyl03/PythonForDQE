922556707d5657236a408bbcf86e86c52d4e44c3
Lily03 <lilyllilyl03@gmail.com>
2025-06-18 16:50:10 +0400

    Python Hw2.1

--- PythonForDQE_hw_2.py
+++ PythonForDQE_hw_2.py
@@ -1,6 +1,14 @@
 import random as rand
 import string
 
+"""
+1. create a list of random number of dicts (from 2 to 10)
+dict's random numbers of keys should be letter,
+dict's values should be a number (0-100),
+example: [{'a': 5, 'b': 7, 'g': 11}, {'a': 3, 'c': 35, 'g': 42}]
+
+"""
+
 dicts=[]
 for i in range(rand.randint(2,10)):
     num_keys=rand.randint(2,10)
